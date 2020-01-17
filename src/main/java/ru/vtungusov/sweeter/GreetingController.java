f7c6830992353a3cfa80adc2c573be5a714a6af8
Vasily Tungusov <vasilt86@mail.ru>
2020-01-17 15:26:25 +0700

    add base security template

--- src/main/java/ru/vtungusov/sweeter/GreetingController.java
+++ src/main/java/ru/vtungusov/sweeter/GreetingController.java
@@ -15,22 +15,19 @@ public class GreetingController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         messageRepo.save(new Message(text, tag));
 
