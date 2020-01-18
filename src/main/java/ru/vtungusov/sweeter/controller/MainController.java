49428e5bc4dbe8a06558109206c80e790b3e8779
Vasily Tungusov <vasilt86@mail.ru>
2020-01-18 14:35:21 +0700

    add field 'author' to Message from another table, related changes, make Security via ORM

--- src/main/java/ru/vtungusov/sweeter/controller/MainController.java
+++ src/main/java/ru/vtungusov/sweeter/controller/MainController.java
@@ -1,11 +1,13 @@
 package ru.vtungusov.sweeter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.vtungusov.sweeter.domain.Message;
+import ru.vtungusov.sweeter.domain.User;
 import ru.vtungusov.sweeter.repos.MessageRepo;
 
 import java.util.Map;
@@ -28,8 +30,11 @@ public class MainController {
     }
 
     @PostMapping("main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        messageRepo.save(new Message(text, tag));
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model) {
+        messageRepo.save(new Message(text, tag, user));
 
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
