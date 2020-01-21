ce399d1f297c053b857d45a6093640dc091d3015
Vasily Tungusov <vasilt86@mail.ru>
2020-01-21 21:09:05 +0700

    add profile with selfchanges, related refactor

--- src/main/java/ru/vtungusov/sweeter/controller/UserController.java
+++ src/main/java/ru/vtungusov/sweeter/controller/UserController.java
@@ -2,31 +2,30 @@ package ru.vtungusov.sweeter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.vtungusov.sweeter.domain.Role;
 import ru.vtungusov.sweeter.domain.User;
-import ru.vtungusov.sweeter.repos.UserRepo;
+import ru.vtungusov.sweeter.service.UserService;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
-    UserRepo userRepo;
+    UserService userService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model) {
-        model.addAttribute("users", userRepo.findAll());
+        model.addAttribute("users", userService.findAll());
         return "userList";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
@@ -34,23 +33,31 @@ public class UserController {
         return "userEdit";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping()
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
             @RequestParam("userId") User user) {
-        user.setUsername(username);
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-
-        user.getRoles().clear();
-        for (String key : form.keySet()) {
-            if (roles.contains(key))
-                user.getRoles().add(Role.valueOf(key));
-        }
-
-        userRepo.save(user);
+        userService.saveUser(user, username, form);
         return "redirect:/user";
     }
+
+    @GetMapping("profile")
+    public String getProfile(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String update(@AuthenticationPrincipal User user,
+                         @RequestParam String password,
+                         @RequestParam String email) {
+        userService.updateProfile(user, password, email);
+
+        return "redirect:/user/profile";
+    }
+
 }
--- src/main/java/ru/vtungusov/sweeter/service/UserService.java
+++ src/main/java/ru/vtungusov/sweeter/service/UserService.java
@@ -10,8 +10,8 @@ import ru.vtungusov.sweeter.domain.Role;
 import ru.vtungusov.sweeter.domain.User;
 import ru.vtungusov.sweeter.repos.UserRepo;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -39,6 +39,11 @@ public class UserService implements UserDetailsService {
 
         userRepo.save(user);
 
+        sendActivationCode(user);
+        return true;
+    }
+
+    private void sendActivationCode(User user) {
         if (!StringUtils.isEmpty(user.getEmail())) {
             String message = String.format("Hello, %s \n" +
                             "Welcome to Sweeter. Please, visit next link for account activation: http://localhost:8080/activate/%s",
@@ -47,7 +52,6 @@ public class UserService implements UserDetailsService {
             );
             mailSender.send(user.getEmail(), "Activation code", message);
         }
-        return true;
     }
 
     public boolean activateUser(String code) {
@@ -60,5 +64,49 @@ public class UserService implements UserDetailsService {
         user.setActivationCode(null);
         userRepo.save(user);
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key))
+                user.getRoles().add(Role.valueOf(key));
+        }
+
+        userRepo.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged = (email != null && !email.equals(userEmail)) ||
+                (userEmail != null && !userEmail.equals(email));
+
+        if (isEmailChanged) {
+            user.setEmail(email);
+
+            if (!StringUtils.isEmpty(email)) {
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
+
+        userRepo.save(user);
+
+        if (isEmailChanged) {
+            sendActivationCode(user);
+        }
     }
 }
