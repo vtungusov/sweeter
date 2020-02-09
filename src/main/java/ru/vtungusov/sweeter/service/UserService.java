848b73bbb86f8ab8e5b276501391d6a1d6ebe9ee
Vasily Tungusov <vasilt86@mail.ru>
2020-02-09 23:11:08 +0700

    add subscriptions ane subscribers

--- src/main/java/ru/vtungusov/sweeter/service/UserService.java
+++ src/main/java/ru/vtungusov/sweeter/service/UserService.java
@@ -119,4 +119,16 @@ public class UserService implements UserDetailsService {
             sendActivationCode(user);
         }
     }
+
+    public void subscribe(User currentUser, User user) {
+        user.getSubscribers().add(currentUser);
+
+        userRepo.save(user);
+    }
+
+    public void unsubscribe(User currentUser, User user) {
+        user.getSubscribers().remove(currentUser);
+
+        userRepo.save(user);
+    }
 }
